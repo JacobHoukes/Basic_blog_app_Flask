06f163b9cc19530bdafa01c83278538faebfd0ce
Jacob <jayhoukes@gmail.com>
2025-05-02 13:01:38 +0200

    handling POST requests added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import os.path
+
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 blog_posts = [
@@ -19,5 +21,35 @@ def index():
     return render_template('index.html', posts=blog_posts)
 
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        if os.path.exists('blog_posts.json'):
+            with open("blog_posts.json", "r") as file:
+                blog_posts = json.load(file)
+        else:
+            blog_posts = []
+
+        new_id = max((post['id'] for post in blog_posts), default=0) + 1
+
+        new_post = {
+            'id': new_id,
+            'author': author,
+            'title': title,
+            'content': content
+        }
+
+        blog_posts.append(new_post)
+        with open('blog_posts.json', 'w') as file:
+            json.dump(blog_posts, file, indent=4)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
